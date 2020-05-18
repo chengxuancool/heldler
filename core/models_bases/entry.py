70d524b88e4c1d8ea98cae65e12d41fc31ee0773
tianfeng.li <tianfeng.li@ucarinc.com>
2020-05-18 18:12:32 +0800

    1. add admin url and its core directory and files. 2. add content field into model.

--- core/models_bases/entry.py
+++ core/models_bases/entry.py
@@ -12,6 +12,7 @@ class BaseEntry(models.Model):
     STATUS_CHOICES = ((0, 'draft'),
                       (1, 'hidden'),
                       (2, 'published'))
+
     title = models.CharField('title', max_length=255)
     slug = models.SlugField(
         'slug', max_length=255,
@@ -28,8 +29,11 @@ class BaseEntry(models.Model):
         db_index=True, default=timezone.now,
         help_text="Used to build the entry's URL.")
 
+    last_update = models.DateTimeField(
+        'last update date', default=timezone.now())
+
     object = models.Manager()
-    published  = EntryPublishedManager()
+    published = EntryPublishedManager()
 
     class Meta:
         """
@@ -38,9 +42,26 @@ class BaseEntry(models.Model):
         abstract = True
         ordering = ['-publication_date']
         get_latest_by = 'publication_date'
+        verbose_name = 'entry'
+        verbose_name_plural = 'entries'
+        # index_together = []
+        permissions = (('can_view_all', 'Can view all entries'),
+                       ('can_change_status', 'Can change status'),
+                       ('can_change_author', 'Can change authors(s)'))
 
 
-class AbstractEntry(BaseEntry):
+class ContentEntry(models.Model):
+    """
+    Abstract content model class defined fields and methods
+    """
+    content = models.TextField('content', blank=True)
+
+    class Meta:
+        abstract = True
+
+class AbstractEntry(
+        BaseEntry,
+        ContentEntry):
     """
     abstract entry model class assembling
     all the abstract entry model in this class
